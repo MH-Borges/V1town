391bdb0f311840a4d86cdf32e532304902c25d37
Matheus Henrique <Matheuinhosilva.borges@hotmail.com>
2021-07-21 18:02:10 -0300

    project page N animation

---
+++ js/JqueryAPP.js
@@ -0,0 +1,134 @@
+$(".theme-switch").on("click", () => {
+    $("body").toggleClass("light-theme");
+    var dark = $("#dark").text();
+    if (dark.trim() === 'Dark Mode') {
+        light = '<span id="lightIn">Light Mode<span>';  
+        $("#darkIn").remove();
+        $("#light").append(light);
+    }
+    if (dark.trim() === 'Light Mode') {
+        DarkIn = '<span id="darkIn">Dark Mode<span>';  
+        $("#lightIn").remove();
+        $("#dark").append(DarkIn);
+    }
+});
+
+$("#activelogo").on("click", () => {
+    $(".Menu").toggleClass("actived");
+    $(".content").toggleClass("actived");
+    $(".iconFts").toggleClass("actived");  
+});
+
+$(".iconFts").on("click", () => {
+    var getClass = $('.iconsFT').attr('class');
+    if (getClass === 'iconsFT') {
+        $(".five").remove();
+        $(".six").remove();
+        $(".iconsFT").addClass("metade");
+        $(".FotoBx").addClass("metade");
+        $(".FotoBx").removeClass("none");
+    }
+    if (getClass === 'iconsFT metade') {
+        $(".iconsFT").removeClass("metade");
+        $(".two").remove();
+        $(".three").remove();
+        $(".four").remove();
+        $(".iconsFT").addClass("cem");
+        $(".FotoBx").removeClass("metade");
+        $(".FotoBx").addClass("cem");
+    }
+    if (getClass === 'iconsFT cem') {
+        $(".iconsFT").removeClass("cem");
+        two = '<div class="iconsFT two"></div>';
+        three = '<div class="iconsFT three"></div>';
+        four = '<div class="iconsFT four"></div>';
+        five = '<div class="iconsFT five"></div>';
+        six = '<div class="iconsFT six"></div>';
+        $(".iconFts").append(two, three, four, five, six);
+        $(".FotoBx").removeClass("cem");
+        $(".hide").addClass("none");
+    }
+});
+
+$(".AbelhaTeste").on("click", () => {
+    var abelha = $("#Abelha").text();
+    if (abelha.trim() === 'Abelha') {
+        abelha = '<span id="TesteAgora"> Clique aqui e teste </span>';
+        TesteMuda = '<li id="resetAbelha"> Resetar </li>';  
+        $("#Abelha").remove();
+        $("#testAbelha").append(abelha);
+        $("#MudaTeste").remove();
+        $(".AbelhaTeste").append(TesteMuda);
+    }
+    else{
+        abelha = '<span id="Abelha"> Abelha </span>'; 
+        TesteMuda = '<li id="MudaTeste">Testar</li>'; 
+        
+        $("#Abelha").remove();
+        $("#MudaTeste").remove();
+
+        $("#TesteAgora").remove();
+        $("#testAbelha").append(abelha);
+        $("#resetAbelha").remove();
+        $(".AbelhaTeste").append(TesteMuda);
+        $("#testAbelha").removeClass("regular");
+        $('#testAbelha').addClass("bold");
+        $('#testAbelha').css("font-size", "4vw");
+    }
+});
+
+$("#type").on("click", () => {
+    var select = $("#type option:selected").val();
+    if(select === 'bold'){
+        $("#testAbelha").removeClass("regular");
+        $('#testAbelha').addClass("bold");
+        $("#linkDownload").attr("href", "font/abelhagorda.otf");
+    }
+    if(select === 'regular'){
+        $("#testAbelha").removeClass("bold");
+        $('#testAbelha').addClass("regular");
+        $("#linkDownload").attr("href", "font/abelharegular.otf");
+    }
+});
+
+$("#slider").on("input",function () {
+    $('#testAbelha').css("font-size", $(this).val() + "px");
+});
+
+$(document).ready(()=> {
+    $(".TradicionalFt").mouseenter(()=> {
+        $('.Tradicional').addClass("none");
+        $('.TradicionalOn').removeClass("none");
+    })
+    .mouseleave(()=> {
+        $('.Tradicional').removeClass("none");
+        $('.TradicionalOn').addClass("none");
+    });
+
+    $(".DecorativaFt").mouseenter(()=> {
+        $('.Decorativa').addClass("none");
+        $('.DecorativaOn').removeClass("none");
+    })
+    .mouseleave(()=> {
+        $('.Decorativa').removeClass("none");
+        $('.DecorativaOn').addClass("none");
+    });
+
+    $(".LigaturasFt").mouseenter(()=> {
+        $('.Ligaturas').addClass("none");
+        $('.LigaturasOn').removeClass("none");
+    })
+    .mouseleave(()=> {
+        $('.Ligaturas').removeClass("none");
+        $('.LigaturasOn').addClass("none");
+    });
+    
+    $(".SetasFt").mouseenter(()=> {
+        $('.Setas').addClass("none");
+        $('.SetasOn').removeClass("none");
+    })
+    .mouseleave(()=> {
+        $('.Setas').removeClass("none");
+        $('.SetasOn').addClass("none");
+    });
+});
---
+++ js/animation.js
@@ -0,0 +1,72 @@
+document.addEventListener("DOMContentLoaded", () => {
+    var tl1 = gsap.timeline();
+    tl1
+    .to('.screen-loading', {
+        duration: 1,
+        opacity: 0,
+        delay: 2.2,
+    })
+    .from(".Logo, .Menu", { 
+        opacity: 0, 
+        y: 50,
+    },"-=1")
+    .to(".Logo, .Menu", {
+        duration: 1, 
+        opacity: 1, 
+        y: 0,
+    })
+    .from(".LongLogo, .MenuInside", {
+        opacity: 0, 
+        y: 50,
+    }, "-=1")
+    .to(".LongLogo, .MenuInside", {
+        duration: 1, 
+        opacity: 1, 
+        y: 0,
+    })
+    .from(".About", {
+        opacity: 0, 
+        y: 50,
+    }, "-=1.5")
+    .to(".About",{ 
+        opacity: 1,
+        duration: 1,
+        y: 0,
+    })
+    .from(".textabout", {
+        opacity: 0, 
+        y: 50,
+    }, "-=3")
+    .to(".textabout",{ 
+        opacity: 1, 
+        duration: 1,
+        y: 0,
+    })
+    .from(".Links, .TabsBx", {
+        opacity: 0, 
+        y: 50,
+    }, "-=3")
+    .to(".Links, .TabsBx",{ 
+        opacity: 1,
+        duration: 1,
+        y: 0,
+    })
+    .from(".link", {
+        opacity: 0, 
+        y: 50,
+    }, "-=3.5")
+    .to(".link",{ 
+        opacity: 1,
+        duration: 1,
+        y: 0,
+    })
+    .from(".dark, .theme-switch", {
+        opacity: 0, 
+        y: 50,
+    }, '-=4')
+    .to(".dark, .theme-switch",{ 
+        opacity: 1, 
+        duration: 1,
+        y: 0,
+    })
+});
